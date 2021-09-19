35a9645e919eda515a912c40f40f9b2d932de508
SdetByGaurav <gaurav17.gaud@gmail.com>
2021-09-19 18:16:09 +0530

    Changes for jenkins demo

--- ActitimeMavenFramework/src/test/java/com/actitime/testscripts/SampleTest.java
+++ ActitimeMavenFramework/src/test/java/com/actitime/testscripts/SampleTest.java
@@ -1,8 +1,12 @@
 package com.actitime.testscripts;
 
+import org.testng.annotations.Test;
+
 public class SampleTest {
 
+	@Test
 	public void checkSampleTest() {
 		System.out.println("sample running");
+		System.out.println("Changes done for jenkins");
 	}
 }
